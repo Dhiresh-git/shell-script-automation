b26d6de7a4284c9196e3f10b443c9a3b0a6918bd
Dhiresh-git <dhireshsonawane@gmail.com>
2023-03-18 16:07:32 +0530

    package istallation in all operating sytem automation script

---
+++ git-install-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Script to install git"
+echo "Installation started"
+if [ "$(uname)" == "Linux" ];
+then
+    echo "this is linux box,installing git"
+    apt-get install git -y
+elif [ "$(uname)" == "Darwin" ];
+then 
+    echo "this is not linux box"
+    echo "this is Macos"
+    brew istall git
+else
+    echo "not istalling"
+fi
